59e7d923e872b9df83922a3e51b5ef45276828bc
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2024-02-20 13:42:48 +0100

    Add files via upload

--- footer/footer.php
+++ footer/footer.php
@@ -1,10 +1,10 @@
 <script src="https://kit.fontawesome.com/7ad21db75c.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="../css/footer.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-<div>
+<div class="foot">
 <p class="fo">JourneyMasters&reg</p>
 <p class="thin">est. 2023.</p>
 <p class="details">Vác, Hungary</p>
