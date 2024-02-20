cefdc5635c1492cfadff2bb5ae73af38a93c3780
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2024-02-20 13:41:16 +0100

    Add files via upload

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -1,12 +1,12 @@
 <script src="https://kit.fontawesome.com/7ad21db75c.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="navbar.css">
+<link rel="stylesheet" href="../css/navbar.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-<div>
-<img src="kislogo2.png" alt="logo">
-<p>JourneyMasters</p>
+<div class="navbar">
+<img src="../img/kislogo2.png" alt="logo">
+<p class="pnav">JourneyMasters</p>
 <ul>
     <li><a href="" id="elso">Tervezés</a></li>
     <li><a href="">Utazások</a></li>
