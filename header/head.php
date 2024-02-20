755e6c07cde2972c9d396b6379b3b01e4ddc9d53
K054D <78139830+K054D@users.noreply.github.com>
2024-02-20 08:51:19 +0100

    Add files via upload

---
+++ header/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/7ad21db75c.js" crossorigin="anonymous"></script>
+    <style>
+        
+    </style>
+</head>
+<body>
+<i class="fa-solid fa-person-walking-luggage"></i>
+</body>
+</html>
