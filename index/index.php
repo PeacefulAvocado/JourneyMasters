722f729d377800af1b83c050ce653073c77ac135
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2024-02-20 13:41:56 +0100

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>JourneyMasters</title>
+</head>
+<body>
+    <?php include('../navbar/navbar.php')?>
+    <?php include('../main/main.php')?>
+    <?php include('../footer/footer.php')?>
+    
+    
+</body>
+</html>
