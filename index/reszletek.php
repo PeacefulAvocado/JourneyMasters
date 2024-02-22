94e36fe1e51a6746ff144e55e946ba0e2bb168ae
K054D <k054.daniel@gmail.com>
2024-02-22 19:08:19 +0100

    Színek definiálása

---
+++ index/reszletek.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/reszletek.css">
+    <link rel="stylesheet" href="../css/footer.css">
+
+    
+    <title>Részletek</title>
+</head>
+<body>
+    <?php include('../templates/navbar.php')?>
+    <?php include('../templates/reszletektemplate.php')?>
+    <?php include('../templates/footer.php')?>
+    
+    
+</body>
+<!--Javascript a végén kell, hogy legyen, csak így működik xd-->
+<?php include_once('../templates/jquery.php')?>
+</html>
---
+++ templates/reszletektemplate.php
@@ -0,0 +1,16 @@
+<?php
+    require_once(__DIR__."/../helpers/dbhandler.php");
+    $dbhandler = new DbHandler();
+   
+?>
+<script src="https://kit.fontawesome.com/7ad21db75c.js" crossorigin="anonymous"></script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+<div class="reszletekmain">
+<div class="reszletek">
+    
+</div>
+</div>
