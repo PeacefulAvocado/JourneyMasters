eba09e2455b8cf977520badabbb71cc6d0f20177
tothjonas <54719329+PeacefulAvocado@users.noreply.github.com>
2024-02-27 13:39:20 +0100

    kosár

--- index/kosar.php
+++ index/kosar.php
@@ -5,9 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/kosar.css">
+    <link rel="stylesheet" href="../css/footer.css">  
     <link rel="icon" href="../img/kislogo_black.png" type="image/x-icon">
     <title>JourneyMasters | Kosár</title>
 </head>
--- templates/kosartemplate.php
+++ templates/kosartemplate.php
@@ -4,30 +4,46 @@
    
 ?>
 <script src='https://kit.fontawesome.com/7ad21db75c.js' crossorigin='anonymous'></script>
-
 <link rel='preconnect' href='https://fonts.googleapis.com'>
 <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
 <link href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' rel='stylesheet'>
 
-<div class="kosar-container">
-    <div class="kosar-head">
-        <h1>Kosaram</h1>
-    </div>
-    <div class="kosar-content">
-        <div class="utazas">
-            <div class="utazas-card">
-                <img src="../img/helyszinimg/Bayview Retreat/1.jpg" alt="hotel-kep" class="hotel-kep"> 
-                <p class="cim">Sydney<p class="stars"></p></p><br>  
-                <p class="utirany">Budapest - Sydney</p>  <br>
-                <p class="date">2014-01-01 - 2014-01-02</p>  
-                <img src="../img/nyil.svg" alt="" class="go-to">
-            </div>
-            <img src="../img/trash.svg" alt="törlés" class = "delete">
-        </div>
-        
-    </div>
-</div>
+<div class="kosarmain">
+<div class="kosar">
+<p class="kosarcim">Kosaram</p>
+<hr class="vonal">
 
- <?php
+
+<div class="utazasok">
+    <p class="kiscim">Félben hagyott foglalásaim</p>
+
+  
+    <form action="../index/foglalas.php" method="post" class="tervezesegyeni">
+      <img src="../img/sydneyproba.jpg" alt="Sydney">
+      <p class="hotelnev">Morocco Central</p>
+      <p class="stars"><i class='fa-solid fa-star'></i></p>
+      <p class="hotelcim">Sydney, Australia 30 Grosvenor St.</p>
+      <p class="fok">3 fő</p>
+      <input type="hidden" name="csomag" value="false">
+      <input type="hidden" name="datum"  value="">
+      <input type="hidden" name="hotelcim" value="">
+      <button class="newbutton" onclick="alert('asda')" type="button"></button>
+      <input type="submit" class="torlesbutton" value="">
+    </form>
+
+
+    <form action="../index/foglalas.php" method="post" class="tervezesegyeni">
+      <img src="../img/sydneyproba.jpg" alt="Sydney">
+      <p class="hotelnev">Morocco Central</p>
+      <p class="stars"><i class='fa-solid fa-star'></i></p>
+      <p class="hotelcim">Sydney, Australia 30 Grosvenor St.</p>
+      <p class="fok">3 fő</p>
+      <input type="hidden" name="csomag" value="false">
+      <input type="hidden" name="datum"  value="">
+      <input type="hidden" name="hotelcim" value="">
+      <button class="newbutton" onclick="alert('asda')" type="button"></button>
+      <input type="submit" class="torlesbutton" value="">
+    </form>
  
- ?>
+</div>
+</div>
