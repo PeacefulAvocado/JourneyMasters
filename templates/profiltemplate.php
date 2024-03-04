c22eb9c1f09f76b58418130369aa029df3b5f914
abence1 <acsbence25@gmail.com>
2024-03-04 13:48:05 +0100

    profil update

--- templates/profiltemplate.php
+++ templates/profiltemplate.php
@@ -1,5 +1,7 @@
 <?php
-  
+  require_once(__DIR__.'/../helpers/dbhandler.php');
+  $dbhandler = new DbHandler();
+
   session_start();
   if(!isset($_SESSION['utasid'])){
       header("Location: ../index/login.php");
@@ -9,14 +11,17 @@
   if (isset($_POST['kijel']))
   {
     $_SESSION['utasid'] = null;
-    echo "<script>alert('Sikeresen kijelentkezett!')</script>";
-    header("Location: ../index/login.php");
-    exit();
+    echo "<script>alert('Sikeresen kijelentkezett!')
+    window.location.href = '../index/login.php';</script>";
   }
+  $email = $dbhandler->getKeresettNoAktiv('userdata', 'email', 'utasid', $_SESSION['utasid'])[0];
+
+  $utazasok = $dbhandler->select("select * from utazas where utasazon = ".$_SESSION['utasid']); 
+
 ?>
 <div class="profilmain">
 <div class="profil">
-<p class="emailcim">dzsonasz@gmail.com</p>
+<p class="emailcim"><?= $email ?></p>
 <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
     <input type="submit" name="kijel" value="Kijelentkezés" class="kijel">
 </form>
@@ -39,21 +44,40 @@
 <div class="utazasok">
   <p class="kiscim">Profilhoz tartozó utazások</p>
 
-  
-    <form action="../index/foglalas.php" method="post" class="tervezesegyeni">
-      <img src="../img/sydneyproba.jpg" alt="Sydney">
-      <p class="hotelnev">Morocco Central</p>
-      <p class="stars"><i class='fa-solid fa-star'></i></p>
-      <p class="hotelcim">Sydney, Australia 30 Grosvenor St.</p>
-      <p class="fok">3 fő</p>
-      <input type="hidden" name="csomag" value="false">
-      <input type="hidden" name="datum"  value="">
-      <input type="hidden" name="hotelcim" value="">
-      <button class="newbutton" onclick="alert('asda')" type="button"></button>
-      <button class="modositasbutton" onclick="alert('asda')" type="button"></button>
-      <input type="submit" class="torlesbutton" value="">
+  <?php
+    if ($utazasok == array())
+    {
+      echo "<p>Ehhez a profilhoz még nem tartoznak utazások!</p>";
+    }
+    else 
+    {
+    for ($i = 0; $i < count($utazasok); $i++)
+    {
+      $celpont = $utazasok[$i]['celpont'];
+      $stars = $dbhandler->getKeresett('helyszin', 'csillag', 'nev', "'$celpont'")[0];
+      $stars_str = "";
+      for ($n = 0; $n < $stars; $n++) { 
+          $stars_str .= "<i class='fa-solid fa-star'></i>";
+      }
+      $cim = $dbhandler->getKeresett('helyszin', 'varos', 'nev', "'$celpont'")[0].", ".$dbhandler->getKeresett('helyszin', 'cim', 'nev', "'$celpont'")[0];
+      $fo = $dbhandler->select("select count(*) as '0' from csoport where csoportid = (select csoportid from csoport where utasid =".$_SESSION['utasid'].");")[0][0];
+      echo "
+      <form action='../index/foglalas.php' method='post' class='tervezesegyeni'>
+      <img src='../img/helyszinimg/".$celpont."/1.jpg' alt='Sydney'>
+      <p class='hotelnev'>".$celpont."</p>
+      <p class='stars'>$stars_str</i></p>
+      <p class='hotelcim'>$cim</p>
+      <p class='fok'>$fo fő</p>
+      <input type='hidden' name='csomag' value='false'>
+      <input type='hidden' name='datum'  value=''>
+      <input type='hidden' name='hotelcim' value=''>
+      <button class='newbutton' onclick='alert('asda')' type='button'></button>
+      <input type='submit' class='torlesbutton' value=''>
     </form>
-
+      ";
+    }
+  }
+  ?>
 
     <form action="../index/foglalas.php" method="post" class="tervezesegyeni inaktiv">
       <img src="../img/sydneyproba.jpg" alt="Sydney">
